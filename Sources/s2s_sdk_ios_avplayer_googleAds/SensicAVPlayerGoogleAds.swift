da47a346f9c0f75b5e9ced4341a9bde7ea6cecb1
KornazovKristiyan <97964609+KornazovKristiyan@users.noreply.github.com>
2024-09-18 23:19:36 +0300

    Delete Sources directory

--- Sources/s2s_sdk_ios_avplayer_googleAds/SensicAVPlayerGoogleAds.swift
+++
@@ -1,7 +0,0 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
-//import Sentry
-//import SentryPrivate
-
-import s2s_sdk_ios_avplayer_googleAds
--- Sources/s2s_sdk_ios_bitmovin/SensicBitmovin.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Kornazov, Kristiyan (GfK) on 21.08.24.
-//
-
-import Foundation
-import s2s_sdk_ios_bitmovin
