a2e14d28208b8603a677e4eae4a514df1e1894be
KornazovKristiyan <97964609+KornazovKristiyan@users.noreply.github.com>
2024-09-18 22:22:24 +0200

    Add files via upload

--- Package.swift
+++ Package.swift
@@ -8,34 +8,11 @@ let package = Package(
     ],
     products: [
         // Define separate products for each target to keep them isolated
-        .library(name: "s2s_sdk_ios_avplayer_googleAds", targets: ["s2s_sdk_ios_avplayer_googleAds"]),
-        .library(name: "s2s_sdk_ios_bitmovin", targets: ["s2s_sdk_ios_bitmovin"]),
-        .library(name: "s2s_sdk_ios_agent_only", targets: ["s2s_sdk_ios_agent_only"])
+        .library(name: "AVPlayer-GoogleAds", targets: ["s2s_sdk_ios_avplayer_googleAds"]),
+        .library(name: "Bitmovin", targets: ["s2s_sdk_ios_bitmovin"]),
+        .library(name: "AgentOnly", targets: ["s2s_sdk_ios_agent_only"])
     ],
     targets: [
-        .target(
-            name: "SensicAVPlayerGoogleAds",
-            dependencies: [
-                .target(name: "s2s_sdk_ios_avplayer_googleAds")
-            ],
-            path: "Sources/s2s_sdk_ios_avplayer_googleAds"
-        ),
-        
-        .target(
-            name: "SensicBitmovin",
-            dependencies: [
-                .target(name: "s2s_sdk_ios_bitmovin")
-            ],
-            path: "Sources/s2s_sdk_ios_bitmovin"
-        ),
-        
-        .target(
-            name: "SensicAgentOnly",
-            dependencies: [
-                .target(name: "s2s_sdk_ios_agent_only")
-            ],
-            path: "Sources/s2s_sdk_ios_agent_only"
-        ),
         .binaryTarget(
             name: "s2s_sdk_ios_avplayer_googleAds",
             path: "./s2s_sdk_ios_avplayer_googleAds.xcframework"
